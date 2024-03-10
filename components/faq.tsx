4b5bc31ea0635cfd7ec38862ac803cef0308c313
denosaurabh <denosaurabh@gmail.com>
2024-03-10 18:28:38 +0530

    Update faq.tsx

--- components/faq.tsx
+++ components/faq.tsx
@@ -8,21 +8,29 @@ export const FAQ = ({ className }: { className?: string }) => {
 
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+          <AccordionTrigger>When will the shipping start after pre-order?</AccordionTrigger>
+          <AccordionContent>
+            Shipping for pre-orders will commence between the 3rd and 4th week of April. Please note
+            that subsequent batches will be shipped out at a much faster pace.
+          </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-2">
-          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionTrigger>Can I cancel my order?</AccordionTrigger>
           <AccordionContent>
-            Yes. It comes with default styles that matches the other components&apos; aesthetic.
+            You have the option to cancel your order before it has been shipped. Additionally, we
+            offer a 7-day return policy after the item has been delivered. For more details on
+            cancellations and returns, please visit our policy page at
+            https://flight.engineering/shipping-return-policy.
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-3">
-          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionTrigger>How much refund will I get on cancellation or return?</AccordionTrigger>
           <AccordionContent>
-            Yes. It&apos;s animated by default, but you can disable it if you prefer.
+            For cancellations, you will receive a 100% refund, excluding the payment processing fee.
+            In the case of returns, we typically refund 80% of the purchase price. However, full
+            refunds may be granted on a case-by-case basis.{' '}
           </AccordionContent>
         </AccordionItem>
       </Accordion>
